4531c0a715447166951006396e3d0eddf2bfbed0
Julian Rabe <mail@julianrabe.com>
2020-06-05 20:13:39 +0200

    Adding commander as CLI lib

--- lib/index.ts
+++ lib/index.ts
@@ -1,46 +1,61 @@
+import {Command} from "commander";
 import Twitch2Ma from "./Twitch2Ma";
 import {Config} from "./Config";
 
 import Fs = require("fs");
 import _ = require("lodash");
 import chalk = require("chalk");
+import packageInformation = require("../package.json");
 
-if (!_.isString(process.argv[2])) {
-    exitWithError(new Error("No config file specified!"));
-}
+const program = new Command();
 
-let jsonObject = failOnErrorOrReturnValue(_.attempt(() => JSON.parse(Fs.readFileSync(process.argv[2], {encoding: "utf-8"}))),
-    new Error(`Cannot read config file ${process.argv[2]}!`));
+program
+    .name("twitch2ma")
+    .description(packageInformation.description)
+    .version(packageInformation.version, "-v, --version", "output the current version")
+    .arguments("[configFile]")
+    .action(main)
+    .parse(process.argv);
 
-const config = failOnErrorOrReturnValue(_.attempt(() => new Config(jsonObject)));
-const twitch2ma = new Twitch2Ma(config);
+function main(configFile: string) {
 
-twitch2ma.onCommandExecuted((channel, user, chatCommand, parameterName, consoleCommand) => {
-
-    parameterName = _.isString(parameterName) ? ` ${parameterName}` : "";
-
-    console.log(chalk`{bgGreen.black  ${channel} }: User {bold ${user}} executed {bold.blue !${chatCommand}${parameterName}}`
-        + (_.isString(consoleCommand) ? chalk` ({magenta ${consoleCommand}}) on the desk.` : '.'));
-});
-
-twitch2ma.onHelpExecuted(((channel, user, helpCommand) => {
-    if (_.isString(helpCommand)) {
-        console.log(chalk`{bgGreen.black  ${channel} }: User {bold ${user}} got help for {bold.blue !${helpCommand}}.`);
-    } else {
-        console.log(chalk`{bgGreen.black  ${channel} }: User {bold ${user}} listed available commands.`);
+    if (!_.isString(configFile)) {
+        exitWithError(new Error("No config file specified!"));
     }
-}));
 
-twitch2ma.onError(exitWithError);
+    let jsonObject = failOnErrorOrReturnValue(_.attempt(() => JSON.parse(Fs.readFileSync(configFile, {encoding: "utf-8"}))),
+        new Error(`Cannot read config file ${configFile}!`));
 
-twitch2ma.start()
-    .then(() => {
-        console.log(chalk`{green Twitch2MA started!}`);
-        console.log(chalk`{green Telnet connected to {bold ${config.ma.user}:***@${config.ma.host}:30000}}`);
-        console.log(chalk`{green Twitch connected to {bold #${config.twitch.channel}}}`);
-        console.log();
-    })
-    .catch(exitWithError);
+    const config = failOnErrorOrReturnValue(_.attempt(() => new Config(jsonObject)));
+    const twitch2ma = new Twitch2Ma(config);
+
+    twitch2ma.onCommandExecuted((channel, user, chatCommand, parameterName, consoleCommand) => {
+
+        parameterName = _.isString(parameterName) ? ` ${parameterName}` : "";
+
+        console.log(chalk`{bgGreen.black  ${channel} }: User {bold ${user}} executed {bold.blue !${chatCommand}${parameterName}}`
+            + (_.isString(consoleCommand) ? chalk` ({magenta ${consoleCommand}}) on the desk.` : '.'));
+    });
+
+    twitch2ma.onHelpExecuted(((channel, user, helpCommand) => {
+        if (_.isString(helpCommand)) {
+            console.log(chalk`{bgGreen.black  ${channel} }: User {bold ${user}} got help for {bold.blue !${helpCommand}}.`);
+        } else {
+            console.log(chalk`{bgGreen.black  ${channel} }: User {bold ${user}} listed available commands.`);
+        }
+    }));
+
+    twitch2ma.onError(exitWithError);
+
+    twitch2ma.start()
+        .then(() => {
+            console.log(chalk`{green Twitch2MA started!}`);
+            console.log(chalk`{green Telnet connected to {bold ${config.ma.user}:***@${config.ma.host}:30000}}`);
+            console.log(chalk`{green Twitch connected to {bold #${config.twitch.channel}}}`);
+            console.log();
+        })
+        .catch(exitWithError);
+}
 
 function failOnErrorOrReturnValue(value: any, overrideError?: Error) {
     if (_.isError(value)) {
