95ba4fe88867476d657c13bca3bf15caf64011a5
Julian Rabe <mail@julianrabe.com>
2020-06-11 23:55:15 +0200

    feat: Add sentry

--- cli.js
+++ cli.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
+
+try {
+    const sentry = JSON.parse(fs.readFileSync("./sentry.json"));
+
+    if (sentry) {
+        const Sentry = require("@sentry/node");
+        const packageInformation = require("./package.json");
+        Sentry.init({
+            dsn: sentry.dsn,
+            release: 'twitch2ma@' + packageInformation.version
+        });
+    }
+} catch(ignored){
+}
+
 require("./dist/lib/index").main();
