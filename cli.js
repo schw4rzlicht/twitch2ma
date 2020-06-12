e600661258e085fa0b5a6a88f43f70c86ace0648
Julian Rabe <mail@julianrabe.com>
2020-06-12 05:23:58 +0200

    Add information to sentry

--- cli.js
+++ cli.js
@@ -6,11 +6,28 @@ try {
     const sentry = JSON.parse(fs.readFileSync("./sentry.json"));
 
     if (sentry) {
+
         const Sentry = require("@sentry/node");
+        const os = require("os");
         const packageInformation = require("./package.json");
+
         Sentry.init({
             dsn: sentry.dsn,
-            release: 'twitch2ma@' + packageInformation.version
+            release: 'twitch2ma@' + packageInformation.version,
+            integrations: [
+                new Sentry.Integrations.OnUncaughtException(),
+                new Sentry.Integrations.OnUnhandledRejection()
+            ]
+        });
+
+        Sentry.setContext("os", {
+            name: os.platform(),
+            version: os.release()
+        });
+
+        Sentry.setContext("runtime", {
+            name: process.release.name,
+            version: process.version
         });
     }
 } catch(ignored){
