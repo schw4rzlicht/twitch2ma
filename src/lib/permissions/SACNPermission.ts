bc30dd99a54f88c64a49649cdaf4de2dd1a31ae7
Julian Rabe <mail@julianrabe.com>
2020-06-14 02:16:00 +0200

    fix: Null pointer when sACN was never received

--- src/lib/permissions/SACNPermission.ts
+++ src/lib/permissions/SACNPermission.ts
@@ -59,7 +59,7 @@ export default class SACNPermission implements PermissionInstance {
 
             if (sacn) {
                 let universeData = this.universeData.get(sacn.universe);
-                if (_.isInteger(universeData[sacn.channel - 1]) && universeData[sacn.channel - 1] < 255) {
+                if (universeData && _.isInteger(universeData[sacn.channel - 1]) && universeData[sacn.channel - 1] < 255) {
                     permissionCollector.denyPermission("sacn",
                         `@${runtimeInformation.userName}, ${runtimeInformation.config.sacn.lockMessage}`);
                 }
