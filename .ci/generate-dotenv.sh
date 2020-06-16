cc7577ce03d62948d5991f4b5e75fcd600fe9c94
Julian Rabe <mail@julianrabe.com>
2020-06-16 19:02:21 +0200

    fix: Missing .env automatically leads to dev environment

--- .ci/generate-dotenv.sh
+++ .ci/generate-dotenv.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-echo "generate-dotenv.sh started!"      # DEBUG
-
 if [[ "$1" = "undefined" ]]
 then
-    echo "Posting production to .env"   # DEBUG
     echo "NODE_ENV=production" > .env
 else
-    echo "Posting staging to .env"      # DEBUG
     echo "NODE_ENV=staging" > .env
 fi
--- .ci/notify-sentry.sh
+++ .ci/notify-sentry.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "notify-sentry.sh started!"                    # DEBUG
-
 set -a
 source .env
 set +a
@@ -9,9 +7,6 @@ set +a
 curl -sL https://sentry.io/get-cli/ | bash
 
 export SENTRY_RELEASE=$1
-
-echo "Content of SENTRY_RELEASE: $SENTRY_RELEASE"   # DEBUG
-echo "Content of NODE_ENV: $NODE_ENV"               # DEBUG
 
 sentry-cli releases new -p $SENTRY_PROJECT $SENTRY_RELEASE
 sentry-cli releases set-commits $SENTRY_RELEASE --auto
